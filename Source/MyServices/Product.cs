90ade30b140e0024fcfae094aab29234bd752796
Lars Michael <lars.michael.dk@gmail.com>
2021-11-05 16:30:04 +0100

    initial

---
+++ Source/MyServices/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace MyServices
+{
+    using System;
+    using DomainServices.Abstractions;
+
+    public class Product : BaseNamedEntity<Guid>
+    {
+        public Product(Guid id, string name) : base(id, name)
+        {
+        }
+
+        public virtual decimal Price { get; set; }
+    }
+}
