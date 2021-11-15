da76c6dec6905ccc9191aa1bdf0006b5c5a905f8
Lars Michael <lars.michael.dk@gmail.com>
2021-11-15 18:20:09 +0100

    More actions products controller

--- Source/MyServices.WebApi/Controllers/ProductsController.cs
+++ Source/MyServices.WebApi/Controllers/ProductsController.cs
@@ -1,24 +1,49 @@
-﻿namespace MyServices.WebApi.Controllers
+﻿namespace MyServices.WebApi.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/products")]
+public class ProductsController : ControllerBase
 {
-    using Microsoft.AspNetCore.Mvc;
+    private readonly ProductService _productService;
 
-    [ApiController]
-    [Route("api/products")]
-    public class ProductsController : ControllerBase
+    public ProductsController(IProductRepository productRepository)
     {
-        private readonly ProductService _productService;
-
-        public ProductsController(IProductRepository productRepository)
-        {
-            _productService = new ProductService(productRepository);
-        }
-
-        [HttpGet()]
-        [Route("all")]
-        public ActionResult<IEnumerable<Product>> GetAll()
-        {
-            return Ok(_productService.GetAll());
-        }
-        
+        _productService = new ProductService(productRepository);
     }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Product>> GetAll() => Ok(_productService.GetAll());
+
+    [HttpGet]
+    [Route("{id}")]
+    public ActionResult<Product> Get(Guid id) => Ok(_productService.Get(id));
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public ActionResult<Product> Add(ProductDto productDto)
+    {
+        var product = productDto.ToProduct();
+        _productService.Add(product);
+        return CreatedAtAction(nameof(Get), new { id = product.Id }, product);
+    }
+
+    [HttpPut]
+    public ActionResult<Product> Update(ProductDto productDto)
+    {
+        var product = productDto.ToProduct();
+        _productService.Update(product);
+        return Ok(_productService.Get(product.Id));
+    }
+
+    [HttpDelete]
+    [Route("{id}")]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public IActionResult Delete(Guid id)
+    {
+        _productService.Remove(id);
+        return NoContent();
+    }
+
 }
