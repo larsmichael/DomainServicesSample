7e4531d4cf7c28af997a57ee9cf7a2c9686b1828
Lars Michael <lars.michael.dk@gmail.com>
2021-11-18 17:28:14 +0100

    Hide model in Swagger UI

--- Source/MyServices.WebApi/Program.cs
+++ Source/MyServices.WebApi/Program.cs
@@ -18,7 +18,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options => { options.DefaultModelsExpandDepth(-1); });
 }
 
 app.UseExceptionHandling();
